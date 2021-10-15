fc9182a7f82db48c6044f779dd0e9a3995c6cb75
gauthamys <66833388+gauthamys@users.noreply.github.com>
2021-10-15 20:30:39 +0530

    Add files via upload

---
+++ reducer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+cur = None
+out = []
+for line in sys.stdin:
+	line = line.strip().split('\t')
+	nex = int(line[0])
+	dest=int(line[1])
+	#print(cur,dest)
+	if cur==None:
+		cur=nex
+		out.append(dest)
+	else:
+		if nex==cur:
+			out.append(dest)
+		else:
+			#print('node hath changed')
+			print(cur,out,sep=',')
+			cur=nex
+			out=[]
+			out.append(dest)
+print(cur,out,sep=',')
