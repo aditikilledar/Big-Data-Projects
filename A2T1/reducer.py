a2e29bc64f3ec172ca789ec49bbb86bb7bf4614e
Aditi Killedar <aditi.s.killedar@gmail.com>
2021-10-18 11:34:55 +0530

    v

--- A2T1/reducer.py
+++ A2T1/reducer.py
@@ -7,21 +7,26 @@ f = open("v", "a+")
 
 for line in sys.stdin:
 	line = line.strip().split('\t')
-	nex = int(line[0])
+	nex = int(line[0])   
 	dest=int(line[1])
 	#print(cur,dest)
 	if cur==None:
+		# handles the case for the first iteration
 		cur=nex
 		out.append(dest)
 	else:
 		if nex==cur:
+			# adds out points for each same node
 			out.append(dest)
 		else:
-			#print('node hath changed')
+			# node changed; prints out list and switches to the next node
 			print(cur,out,sep=',')
+
+			# writes to file v (inital page ranks)
+			f.write(f"{cur},1")
 			cur=nex
 			out=[]
 			out.append(dest)
-			
+
 print(cur,out,sep=',')
 f.close()
