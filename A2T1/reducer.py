1eea38f6b80b479be2f285f70a5f773a3ec4641a
gauthamys <66833388+gauthamys@users.noreply.github.com>
2021-10-19 21:16:14 +0530

    Update reducer.py

--- A2T1/reducer.py
+++ A2T1/reducer.py
@@ -1,33 +1,42 @@
+
 #!/usr/bin/env python3
 import sys
+import os
 cur = None
-out = []
-
-f = open("v", "a+")
+v_path=sys.argv[1]
+f = open(v_path,'w')
 
 for line in sys.stdin:
 	line = line.strip().split('\t')
+	#print(line)
 	nex = int(line[0])   
 	dest=int(line[1])
 	#print(cur,dest)
 	if cur==None:
 		# handles the case for the first iteration
 		cur=nex
-		out.append(dest)
+		print(f'{cur}\t[{dest}',end='')
+		#out.append(dest)
 	else:
 		if nex==cur:
 			# adds out points for each same node
-			out.append(dest)
+			#out.append(dest)
+			print(f',{dest}',end='')
 		else:
 			# node changed; prints out list and switches to the next node
-			print(cur,out,sep=',')
+			#print(cur,out,sep=',')
 
 			# writes to file v (inital page ranks)
+			#print('writing into file now')
 			f.write(f"{cur},1\n")
 			cur=nex
-			out=[]
-			out.append(dest)
+			print(']')
+			print(f'{cur}\t[{dest}',end='')
+			#out=[]
+			#out.append(dest)
 
-print(cur,out,sep=',')
+print(']')
+#f.write('in file')
+#print('writing into file now')
 f.write(f"{cur},1\n")
 f.close()
