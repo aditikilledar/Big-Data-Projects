d6417f11cbe7a30426939aaaacc6c769803bd270
Aditi Killedar <aditi.s.killedar@gmail.com>
2021-10-22 23:50:19 +0530

    FINAL IT WORKS, with COMMETNS

--- A2T2/mapper.py
+++ A2T2/mapper.py
@@ -26,51 +26,48 @@ for line in sys.stdin:
 	# similarity = embedding(p).embedding(q) / magnitude(p).magnitude(q)
 	# RANK = 0.15 + 0.85(Sum(contribution(q) for q in outgoing list)) 
 	
-	
 	line=line.split('\t')
 	p=int(line[0])
 	p_vec = embedding[str(p)]
 	outgoing=line[1].strip()
 	outgoing = outgoing[1:-1].split(',')
-	#print(outgoing)
 	p_rank = v[p]/len(outgoing)
 		
 	#rank is available from here
-	#print(f'{p},{p},0')
 	for q in outgoing:
-	
 		visited.add(int(q))
-		#print(q)
 		q_vec = embedding[q]
 		dot=0
 		mag_p=0
 		mag_q=0
 		similarity=0
+
+		# calculates magnitude of mag_p
 		for j in range(len(p_vec)):
 			mag_p += p_vec[j]*p_vec[j]
 		mag_p = mag_p**(0.5)
 		
+		# calculates magnitude of mag_q
 		for k in range(len(q_vec)):
 			mag_q += q_vec[k]*q_vec[k]
 		mag_q = mag_q**(0.5)
 		
+		# calculates p.q
 		for i in range(5):
 			dot += float(q_vec[i])*float(p_vec[i])
 		
-		
+		# does dot product fully (divides old dot by |p||q|)
 		if mag_p == 0 or mag_q == 0:
 			similarity=0
 		else:
 			similarity = dot/(mag_p*mag_q)
-			
-			
+	
+		# contribution is rank*similarity
 		cont = p_rank*similarity
 		
-		#if q==100002:
 		print(f'{q}${p}${cont}')
-		#print(mag_p,mag_q,1)
-		#print(p,v)
-		
+
+# handles case of node w no incoming
 for node in v.keys():
 	if node not in visited:
 		print(f'{node}${node}$0')
--- A2T2/reducer.py
+++ A2T2/reducer.py
@@ -11,22 +11,28 @@ for line in sys.stdin:
 	# input - q,p,cont(p,q)
 	# output - print p,rank(p) written back to v1
 	
+	#  yayyy, it works!
+	#  what fixed it was, chnaging the delimiter from "," to "$"
 	line=line.strip().split('$')
 	
 	q=int(line[0])
 	p=int(line[1])
 	cont_pq=float(line[2])
 	
+	# handles CASE1: first line
 	if cur==None:
 		cur=q
+	# for each node
 	if cur==q:
 		rank_q += 0.85*cont_pq
 	else:
+		# when it changes
 		print(f'{cur},{rank_q:.2f}')                         
 		v1.write(f'{cur},{rank_q:.2f}\n')
 		rank_q = 0.15 + (0.85*cont_pq)
 		cur=q
 		
+# prints it 
 print(f'{cur},{rank_q:.2f}')
 v1.write(f'{cur},{rank_q:.2f}')
 v1.close()
