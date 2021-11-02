b9c5d093f52fc7badd258923e858a4e23c4d42b8
Aditi Killedar <aditi.s.killedar@gmail.com>
2021-11-02 21:26:14 +0530

    task1 works, task2 initialized

---
+++ A3/task2.py
@@ -0,0 +1,16 @@
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+import sys
+
+citypath=sys.argv[1]
+globpath=sys.argv[2]
+
+citydf = sqlContext.read.csv(citypath, header=True)
+citydf = df.drop('AverageTemperatureUncertainty','Latitude','Longitude')
+
+globdf = sqlContext.read.csv(globpath, header=True)
+
+globdf = globdf.withColumn("LandAverageTemperature",globdf.LandAverageTemperature.cast('float'))
+citydf = citydf.withColumn("AverageTemperature",citydf.AverageTemperature.cast('float'))
+
+# globdf.show(20)
