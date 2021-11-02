f57ddba0a723448cf36a79a39a9352b03c2cd332
gauthamys <66833388+gauthamys@users.noreply.github.com>
2021-11-02 22:04:26 +0530

    yes

--- A3/task2.py
+++ A3/task2.py
@@ -1,19 +1,42 @@
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
+from pyspark.sql.functions import col
 import sys
-
-citypath=sys.argv[1]
-globpath=sys.argv[2]
 
 sc=SparkContext('local',"task2")
 sqlContext = SQLContext(sc)
 
-citydf = sqlContext.read.csv(citypath, header=True)
-citydf = df.drop('AverageTemperatureUncertainty','Latitude','Longitude')
+citypath=sys.argv[1]
+globpath=sys.argv[2]
 
+citydf = sqlContext.read.csv(citypath, header=True)
 globdf = sqlContext.read.csv(globpath, header=True)
 
-globdf = globdf.withColumn("LandAverageTemperature",globdf.LandAverageTemperature.cast('float'))
-citydf = citydf.withColumn("AverageTemperature",citydf.AverageTemperature.cast('float'))
+globdf = globdf.select(col('dt'),col('LandAverageTemperature'))
 
-# globdf.show(20)
+citydf =  citydf.drop('AverageTemperatureUncertainty','Latitude','Longitude')
+
+globdf =  globdf.withColumn("LandAverageTemperature",globdf.LandAverageTemperature.cast('float'))
+citydf =  citydf.withColumn("AverageTemperature",citydf.AverageTemperature.cast('float'))
+
+cityref=citydf.groupby('dt','Country').max('AverageTemperature')
+#cityref.show(20)
+
+globdf = globdf.withColumnRenamed('dt', 'globdt')
+
+cityref=cityref.join(globdf,cityref.dt==globdf.globdt)
+cityref=cityref.drop('globdt')
+cityref=cityref.withColumnRenamed("max(AverageTemperature)","MAXTEMP")
+#cityref.show(5)
+cityref=cityref.filter(cityref.LandAverageTemperature < cityref.MAXTEMP)
+
+rdd1 = cityref.rdd
+rdd1=rdd1.map(lambda x: (x.Country,1))
+rdd2=rdd1.reduceByKey(lambda a,b: a+b)
+rdd3 = rdd2.sortByKey()
+for element in rdd3.collect():
+    print(element[0], '\t', element[1])
+
+#cityref.show(20)
+#globdf.show(20)
+#citydf.show(20)
